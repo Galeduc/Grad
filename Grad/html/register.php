229ca42112728113745d57ccdf0851d59a76b7b7
Galeduc <81381757+Galeduc@users.noreply.github.com>
2023-11-30 19:31:30 +0100

    Ajout de la nav-bar responsive

--- Grad/html/register.php
+++ Grad/html/register.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>Grad - Enregistrement</title>
     <script src="https://kit.fontawesome.com/59fa4e08aa.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.11.0/mdb.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/style.css">
     <script src="../js/script.js"></script>
